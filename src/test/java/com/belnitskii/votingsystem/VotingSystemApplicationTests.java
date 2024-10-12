97bf887987ee2ebb6039d10c89584d95c1bddb37
Alex Belnitskii <belnitskii@mail.ru>
2024-10-12 14:02:47 +0200

    create project

---
+++ src/test/java/com/belnitskii/votingsystem/VotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belnitskii.votingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
